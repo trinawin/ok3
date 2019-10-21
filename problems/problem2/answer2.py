7a0774e45692bb95d93b67f5cfdebc18929f9bc4
Zachary Pinto <zpinto@uci.edu>
2019-10-20 22:56:16 -0700

    solved question 2

--- problems/problem2/answer2.py
+++ problems/problem2/answer2.py
@@ -5,4 +5,12 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+
+        seen = {}
+        for index, value in enumerate(nums):
+            diff = target - value
+            if diff in seen:
+                return [seen[diff], index]
+            seen[value] = index
+
         return None
